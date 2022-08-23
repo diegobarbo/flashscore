487afef3b55cd83628c14d795c2488cb5e3bcd69
Diego Barbo <diegobarbox@gmail.com>
2022-08-23 07:27:26 -0300

    showing a box

--- cards/urls.py
+++ cards/urls.py
@@ -14,4 +14,8 @@ urlpatterns = [
     path(
         'edit/<int:pk>', views.CardUpdateView.as_view(), name='card-update'
          ),
+    path(
+        'box/<int:box_num>',
+         views.BoxView.as_view(), name='box'
+         ),
 ]
